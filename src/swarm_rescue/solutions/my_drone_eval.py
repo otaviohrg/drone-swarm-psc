a86e641745f11a094750cd6d39a31cef6620f8ba
Gabriel Pereira de Carvalho <nazter57@usp.br>
2023-11-23 00:23:33 +0100

    :memo: wip

--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -1,114 +1,243 @@
-from solutions.my_drone_random import MyDroneRandom
-from typing import Optional
+import math
+import random
 import numpy as np
+from typing import Optional
+from enum import Enum
 
 from spg_overlay.entities.drone_abstract import DroneAbstract
 from spg_overlay.utils.misc_data import MiscData
-from spg_overlay.utils.utils import normalize_angle
+from spg_overlay.entities.drone_distance_sensors import DroneSemanticSensor
+from spg_overlay.utils.utils import normalize_angle, circular_mean
 
-class MyDroneEval(MyDroneRandom):
-    
+from scipy.stats import levy
+
+class MyDroneEval(DroneAbstract):
+
+    def define_message_for_all(self):
+        """
+        Here, we don't need communication...
+        """
+        pass
+
+    class Activity(Enum):
+        """
+        All the states of the drone as a state machine
+        """
+        SEARCHING_WOUNDED = 1
+        GRASPING_WOUNDED = 2
+        SEARCHING_RESCUE_CENTER = 3
+        DROPPING_AT_RESCUE_CENTER = 4
+
+
     def __init__(self,
                  identifier: Optional[int] = None,
                  misc_data: Optional[MiscData] = None,
                  **kwargs):
-        super(MyDroneRandom,self).__init__( identifier=identifier,
+        super().__init__(identifier=identifier,
                          misc_data=misc_data,
                          display_lidar_graph=False,
                          **kwargs)
         
-        self.straight = {"forward": 1.0,
-                            "lateral": 0.0,
-                            "rotation": 0.0,
-                            "grasper": 0}
-        self.right = {"forward": 0.0,
-                        "lateral": 0.0,
-                        "rotation": 1.0,
-                        "grasper": 0}
-        self.rightTrans = {"forward": 0.0,
-                    "lateral": 1,
-                    "rotation": 0,
-                    "grasper": 0}
+        # The state is initialized to searching wounded person
+        self.state = self.Activity.SEARCHING_WOUNDED
+        # values used by the control function
+        self.counterStraight = 0
+        self.counterStopStraight = 0
+        self.angleStopTurning = 0
+        self.isTurningLeft = False
+        self.isTurningRight = False
+        self.colision_memory = 0
         
-        self.left = {"forward": 0.0,
-                        "lateral": 0.0,
-                        "rotation": 1.0,
-                        "grasper": 0}
-        self.strategy = "rw"
-        self.rightWall=0
-    def findWall(self):
-        dist = min(self.lidar_values())
-        if dist < 250:
-            self.rightWall=1
-            print("Seting up")
-            return 
-        return self.straight
-    def RWSetup(self):
-        dist = min(self.lidar_values())
-        theta=np.argmin(self.lidar_values())
-        #print(theta)
-        if 48>theta>42:
-            if 125<self.lidar_values()[45]:
-                return self.rightTrans
-            else:
-                self.rightWall=2
-                print("Following")
-                return
-        return self.right
-    def RWFollow(self):
-        dist = self.lidar_values()[90]
-        if dist<100:
-            self.rightWall=1
-            print("Seting up")
-        track = self.straight.copy()
-        if 125>self.lidar_values()[45]>75:
-            track["lateral"]=-(self.lidar_values()[45]-75)/50
-        if 125<self.lidar_values()[45]:
-            self.rightWall=3
-            print("Turning")
-            return
-        if self.lidar_values()[45]<50:
-            track["lateral"]=(50-self.lidar_values()[45])/50
-        return track
-    def RWTurn(self):
-        if min(self.lidar_values()[45:90])<35:
-            print("End of turn")
-            self.rightWall=1
-            print("Seting up")
-            return
-        mvt=self.rightTrans.copy()
-        mvt["lateral"]=-0.5
-        mvt["rotation"]=-0.25
-        return mvt
-    def RW(self):
-        if self.rightWall==0:
-            return self.findWall()
-        if self.rightWall==1:
-            return self.RWSetup()
-        if self.rightWall==2:
-            return self.RWFollow()
-        if self.rightWall==3:
-            return self.RWTurn()
-        return
-        
+
+    def _is_turning(self):
+            return self.isTurningLeft or self.isTurningRight
+
     def process_lidar_sensor(self):
         """
         Returns True if the drone collided an obstacle
         """
+
+        lidar_sensor = self.lidar()
+        values = lidar_sensor.get_sensor_values()
+        ray_angles = lidar_sensor.ray_angles
+        size = lidar_sensor.resolution
+
+        far_angle_raw = 0
+        if size != 0:
+            # far_angle_raw : angle with the longer distance
+            far_angle_raw = ray_angles[np.argmax(values)]
+
         if self.lidar_values() is None:
             return False
-        collided = False
-        dist = self.lidar_values()[90]
 
-        if dist < 100:
+        collided = False
+        dist = min(self.lidar_values())
+
+        if dist < 20:
             collided = True
 
-        return collided
+        return collided, far_angle_raw
+
+    def process_semantic_sensor(self):
+        """
+        According to his state in the state machine, the Drone will move towards a wound person or the rescue center
+        """
+        command = {"forward": 0.5,
+                   "lateral": 0.0,
+                   "rotation": 0.0}
+        angular_vel_controller_max = 1.0
+
+        detection_semantic = self.semantic_values()
+        best_angle = 0
+
+        found_wounded = False
+        if (self.state is self.Activity.SEARCHING_WOUNDED
+            or self.state is self.Activity.GRASPING_WOUNDED) \
+                and detection_semantic is not None:
+            scores = []
+            for data in detection_semantic:
+                # If the wounded person detected is held by nobody
+                if data.entity_type == DroneSemanticSensor.TypeEntity.WOUNDED_PERSON and not data.grasped:
+                    found_wounded = True
+                    v = (data.angle * data.angle) + \
+                        (data.distance * data.distance / 10 ** 5)
+                    scores.append((v, data.angle, data.distance))
+
+            # Select the best one among wounded persons detected
+            best_score = 10000
+            for score in scores:
+                if score[0] < best_score:
+                    best_score = score[0]
+                    best_angle = score[1]
+
+        found_rescue_center = False
+        is_near = False
+        angles_list = []
+        if (self.state is self.Activity.SEARCHING_RESCUE_CENTER
+            or self.state is self.Activity.DROPPING_AT_RESCUE_CENTER) \
+                and detection_semantic:
+            for data in detection_semantic:
+                if data.entity_type == DroneSemanticSensor.TypeEntity.RESCUE_CENTER:
+                    found_rescue_center = True
+                    angles_list.append(data.angle)
+                    is_near = (data.distance < 50)
+
+            if found_rescue_center:
+                best_angle = circular_mean(np.array(angles_list))
+
+        if found_rescue_center or found_wounded:
+            # simple P controller
+            # The robot will turn until best_angle is 0
+            kp = 2.0
+            a = kp * best_angle
+            a = min(a, 1.0)
+            a = max(a, -1.0)
+            command["rotation"] = a * angular_vel_controller_max
+
+            # reduce speed if we need to turn a lot
+            if abs(a) == 1:
+                command["forward"] = 0.2
+
+        if found_rescue_center and is_near:
+            command["forward"] = 0
+            command["rotation"] = random.uniform(0.5, 1)
+
+        return found_wounded, found_rescue_center, command
 
     def control(self):
 
-        
-        comm=self.right
-        if self.strategy=="rw":
-            comm=self.RW()
-        return comm
+        command = {"forward": 0.0,
+                   "lateral": 0.0,
+                   "rotation": 0.0,
+                   "grasper": 0}
+
+        found_wounded, found_rescue_center, command_semantic = self.process_semantic_sensor()
+
+        #############
+        # TRANSITIONS OF THE STATE MACHINE
+        #############
+
+        if self.state is self.Activity.SEARCHING_WOUNDED and found_wounded:
+            self.state = self.Activity.GRASPING_WOUNDED
+
+        elif self.state is self.Activity.GRASPING_WOUNDED and self.base.grasper.grasped_entities:
+            self.state = self.Activity.SEARCHING_RESCUE_CENTER
+
+        elif self.state is self.Activity.GRASPING_WOUNDED and not found_wounded:
+            self.state = self.Activity.SEARCHING_WOUNDED
+
+        elif self.state is self.Activity.SEARCHING_RESCUE_CENTER and found_rescue_center:
+            self.state = self.Activity.DROPPING_AT_RESCUE_CENTER
+
+        elif self.state is self.Activity.DROPPING_AT_RESCUE_CENTER and not self.base.grasper.grasped_entities:
+            self.state = self.Activity.SEARCHING_WOUNDED
+
+        elif self.state is self.Activity.DROPPING_AT_RESCUE_CENTER and not found_rescue_center:
+            self.state = self.Activity.SEARCHING_RESCUE_CENTER
+
+        #print("state: {}, can_grasp: {}, grasped entities: {}".format(self.state.name, self.base.grasper.can_grasp, self.base.grasper.grasped_entities))
+
+        ##########
+        # COMMANDS FOR EACH STATE
+        # Searching randomly, but when a rescue center or wounded person is detected, we use a special command
+        ##########
+
+        def explore():
+
+            command_straight = {"forward": 0.8,
+                            "rotation": 0.0}
+            command_turn_left = {"forward": 0.0,
+                                "rotation": 1.0}
+            command_turn_right = {"forward": 0.0,
+                                "rotation": -1.0}
+            
+            collided, far_angle_raw = self.process_lidar_sensor()
+            self.colision_memory -= 1
+            if(collided and self.colision_memory <= 0):
+                self.colision_memory = 20
+            
+            diff_angle = normalize_angle(self.angleStopTurning - self.measured_compass_angle())
+
+            def levy_step(scale, beta):
+                return scale*(random.uniform(0, 1)**(-beta) - 1)
+
+            if(((not self._is_turning()) and (self.counterStraight >= self.counterStopStraight)) or self.colision_memory==20): #compute next step
+                if(self.colision_memory == 20):
+                    self.angleStopTurning = random.uniform(0.9, 1)*far_angle_raw
+                else:
+                    self.angleStopTurning = random.uniform(-math.pi, math.pi)
+                self.isTurningLeft = (self.angleStopTurning > 0)
+                self.isTurningRight = (self.angleStopTurning < 0)
+                self.counterStraight = 0
+                self.counterStopStraight = levy_step(10 + far_angle_raw/math.pi, 0.75)
+                #self.counterStopStraight = levy.rvs(size = 1)
+
+            if(self._is_turning()):
+                if(abs(diff_angle) < 0.2): #stop turning, lets walk
+                    self.isTurningLeft = False
+                    self.isTurningRight = False
+                else:
+                    if(self.isTurningLeft):
+                        return command_turn_left
+                    return command_turn_right
+
+            self.counterStraight += 1 #walk straight
+            return command_straight
+
+        if self.state is self.Activity.SEARCHING_WOUNDED:
+            command = explore()
+            command["grasper"] = 0
+
+        elif self.state is self.Activity.GRASPING_WOUNDED:
+            command = command_semantic
+            command["grasper"] = 1
+
+        elif self.state is self.Activity.SEARCHING_RESCUE_CENTER:
+            command = explore()
+            command["grasper"] = 1
+
+        elif self.state is self.Activity.DROPPING_AT_RESCUE_CENTER:
+            command = command_semantic
+            command["grasper"] = 1
+
+        return command
