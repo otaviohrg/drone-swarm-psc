54aee81d3f2f025c74ff8b8607395d163575866c
Gabriel Pereira de Carvalho <nazter57@usp.br>
2024-03-26 22:46:34 +0100

    :memo: refactoring

--- src/swarm_rescue/solutions/process_gps.py
+++ src/swarm_rescue/solutions/process_gps.py
@@ -1,9 +1,6 @@
 def get_gps_values(drone):
     '''Return GPS values if available, else use no gps strategy'''
     if drone.gps_is_disabled():
-        return no_gps_strategy(drone)
+        return None, None
     else:
         return drone.measured_gps_position()[0], drone.measured_gps_position()[1]
-
-def no_gps_strategy(drone):
-    return 0, 0
